fa7cc369fa7cf1270c52c3693266678c36816789
Bram den Teuling <bramdenteuling@gmail.com>
2011-11-07 15:26:46 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Slimovation::Application.config.secret_token = '4fb7065075fba01a8020cae408d69d722341112213da3d32831ce783e51a181feee3c16dd616afc6ac0a9d27724db8daf5fbd71e1d16ba4438cc5137417ac0ed'
